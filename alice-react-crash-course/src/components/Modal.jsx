2fcdd3712caceab07bcbaa91d08c78db75ff7d25
Alice M <65421566+amartinarias@users.noreply.github.com>
2025-05-16 14:56:33 +0100

    Update Modal.jsx

--- alice-react-crash-course/src/components/Modal.jsx
+++ alice-react-crash-course/src/components/Modal.jsx
@@ -1,12 +1,22 @@
-// Modal.jsx
 import classes from './Modal.module.css';
 
 function Modal(props) {
-    return (<>
-        <div className={classes.backdrop} onClick={props.onClose}>
-            <dialog open className={classes.modal}>{props.children}</dialog>
-        </div>
-    </>)
+    function handleBackdropClick(event) {
+        // Only close if the click is directly on the backdrop, not on its children
+        console.log("test", event.target, event.currentTarget);
+        console.log(event);
+        if (event.target === event.currentTarget) {
+            props.onClose();
+        }
+    }
+
+    return (
+        <>
+            <div className={classes.backdrop} onClick={handleBackdropClick}>
+                <dialog open className={classes.modal}>{props.children}</dialog>
+            </div>
+        </>
+    )
 }
 
 export default Modal;
